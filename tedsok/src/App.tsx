8d16360abbb3a9c5e5d132e933706c2fa86b19f9
HansPanzer2137 <htopolski12@gmail.com>
2023-03-02 09:22:43 +0100

    nigga

---
+++ tedsok/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { FC, useState } from 'react';
+
+
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
